a7a2b9aad63ae592cc7b5b408cad50e32ac56559
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-02-13 11:13:48 +0100

    [#1] Update Servername Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ subprojects {
 }
 
 paperweight {
-    serverProject = project(":forktest-server")
+    serverProject = project(":onelitepaper-server")
 
     remapRepo = paperMavenPublicUrl
     decompileRepo = paperMavenPublicUrl
@@ -62,10 +62,10 @@ paperweight {
     usePaperUpstream(providers.gradleProperty("paperRef")) {
         withPaperPatcher {
             apiPatchDir = layout.projectDirectory.dir("patches/api")
-            apiOutputDir = layout.projectDirectory.dir("forktest-api")
+            apiOutputDir = layout.projectDirectory.dir("onelitepaper-api")
 
             serverPatchDir = layout.projectDirectory.dir("patches/server")
-            serverOutputDir = layout.projectDirectory.dir("forktest-server")
+            serverOutputDir = layout.projectDirectory.dir("onelitepaper-server")
 
         }
         patchTasks.register("generatedApi") {
@@ -82,7 +82,7 @@ paperweight {
 //
 
 tasks.generateDevelopmentBundle {
-    apiCoordinates = "com.example.paperfork:forktest-api"
+    apiCoordinates = "net.onelitefeather.onelitepaper:onelitepaper-api"
     mojangApiCoordinates = "io.papermc.paper:paper-mojangapi"
     libraryRepositories = listOf(
         "https://repo.maven.apache.org/maven2/",
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,6 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
-rootProject.name = "forktest"
+rootProject.name = "OneLitePaper"
 
-include("forktest-api", "forktest-server")
+include("onelitepaper-api", "onelitepaper-server")
